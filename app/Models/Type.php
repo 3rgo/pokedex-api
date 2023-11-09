eb2b633e28be4797ea8d92e58372d70097c7aa86
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-11-09 14:17:39 +0100

    Type model and migration

---
+++ app/Models/Type.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'array',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Type;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $types = [];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Import types
+        $json = json_decode(file_get_contents(database_path('seeders/fixtures/types.json')), true);
+        foreach ($json as $item) {
+            $type = Type::create([
+                'name'  => Arr::only($item['name'], ['fr', 'en']),
+                'image' => $item['sprites']
+            ]);
+            $types[$item['name']['fr']] = $type->fresh();
+        }
     }
 }
