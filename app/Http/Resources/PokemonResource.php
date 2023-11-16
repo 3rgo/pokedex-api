cb39898211b62bdabda82f718405456bf41dd2a6
Romain GONÇALVES <romain.goncalves@gmail.com>
2023-11-16 14:02:37 +0100

    Adds missing pokemon ID

--- app/Http/Resources/PokemonResource.php
+++ app/Http/Resources/PokemonResource.php
@@ -15,6 +15,7 @@ class PokemonResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'          => $this->id,
             'generation'  => $this->generation,
             'name'        => $this->name,
             'image'       => $this->image,
